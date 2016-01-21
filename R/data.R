81705dd7264da23033b43d1aca5f2e902f152655
Richard Wilkinson <rich.d.wilkinson@gmail.com>
2016-01-21 19:23:36 +0000

    Removed old reference to embankment data

--- R/data.R
+++ R/data.R
@@ -12,16 +12,3 @@
 #' @source MATLAB; load Data_TransProb
 "FinancialRating"
 
-
-#'
-#' Embankment data
-#' 
-#' A dataset containing 178515 ratings of 97076 unique embankment assets
-#'
-#' @format A data frame with 178515 rows and 3 columns:
-#' \describe{
-#'   \item{ID}{Unique asset ID}
-#'   \item{Date}{Inspection date in format \%d-\%b-\%Y}
-#'   \item{EHC}{Earthworks hazard category: a factor with value in 'A', 'B','C','D', 'E'}
-#' }
-"Embankment"
